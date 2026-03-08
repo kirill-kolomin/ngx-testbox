d2b224e7fc1337a9d35e4904b356b1e472afdaa4
kirill-kolomin <kkolomin.w@gmail.com>
2026-03-08 21:48:03 +0300

    WIP

---
+++ projects/tour-of-heroes/features/step_definitions/stepdefs.js
@@ -0,0 +1,18 @@
+import assert from 'assert';
+import { Given, When, Then }  from '@cucumber/cucumber';
+
+function isItFriday(today) {
+  return today === 'Friday' ? 'TGIF': 'Nope';
+}
+
+Given('today is {string}', function (givenDay) {
+  this.today = givenDay;
+});
+
+When('I ask whether it\'s Friday yet', function () {
+  this.actualAnswer = isItFriday(this.today);
+});
+
+Then('I should be told {string}', function (expectedAnswer) {
+  assert.strictEqual(this.actualAnswer, expectedAnswer);
+});
