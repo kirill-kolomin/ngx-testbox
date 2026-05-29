e40d99d44be1a6be1fb8e2e49a8d23c9662cf7b1
kirill-kolomin <kkolomin.w@gmail.com>
2026-05-29 19:34:22 +0100

    Remove getRequestsFromQueue from public api

--- projects/ngx-testbox/testing/src/public_api.ts
+++ projects/ngx-testbox/testing/src/public_api.ts
@@ -7,6 +7,5 @@ export {collectHttpCallsAsync} from './stabilize-fixture/async/collect-http-call
 export {runTasksUntilStableAsync, type RunTasksUntilStableAsyncParams, COMPONENT_LONG_RUN_TIMEOUT} from './stabilize-fixture/async/run-tasks-until-stable-async'
 export {DebugElementHarness} from './debug-element-harness';
 export {predefinedHttpCallInstructions} from './predefined-http-call-instructions';
-export {getRequestsFromQueue} from './get-requests-from-queue';
 export {type CommonStabilizationParams} from './interfaces/common-stabilization-params';
 export {type EndpointPath, type ResponseGetter, type HttpCallChecker, type HttpCallInstruction} from './interfaces/http-call';
